86dcdc1580c3e32639e9862441e676eaa28a3511
ColdSkull5 <halocelmejor@gmail.com>
2022-06-30 00:37:42 -0500

    Test Corredores Davivienda

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Corredores.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Proveedores()
+        {
+            ViewBag.Message = "Proveedores.";
+
+            return View();
+        }
+
+        //public ActionResult Cliente()
+        //{
+        //    ViewBag.Message = "Cliente.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Productos()
+        //{
+        //    ViewBag.Message = "Productos.";
+
+        //    return View();
+        //}
+
+        public ActionResult Contabilidad()
+        {
+            ViewBag.Message = "Contabilidad.";
+
+            return View();
+        }
+    }
+}
